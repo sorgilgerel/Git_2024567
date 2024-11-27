fe8b7654f7e0f2d93649f1b17a757d04d50beab9
sorgilgerel <98004417+sorgilgerel@users.noreply.github.com>
2024-11-27 14:51:13 +0000

    added a simple print

--- src/git_2024567/Git_2024567.java
+++ src/git_2024567/Git_2024567.java
@@ -15,6 +15,7 @@ public class Git_2024567 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Orgilgerel-2024567");
     }
     
 }
